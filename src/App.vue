1d73225db3a4cebe9c4ce385c611c28a58e49236
oinaati <natimartins2007@hotmail.com>
2022-04-15 12:12:30 -0300

    conectando-api

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>{{ titulo }}</h1>
 
     <ul>
-      <li v-for='(foto, i) of fotos' :key='i'>
+      <li v-for="(foto, i) of fotos" :key="i">
         <img :src="foto.url" :alt="foto.titulo" />
       </li>
     </ul>
@@ -15,17 +15,15 @@ export default {
   data() {
     return {
       titulo: "Paisagens",
-      fotos: [
-        {
-          url: "https://blog.nascenteazul.com.br/wp-content/uploads/2021/07/fernando-de-noronha-paisagens-naturais-de-praia.png",
-          titulo: "praia",
-        },
-        {
-          url: "https://www.helenabordon.com/wp-content/uploads/2014/11/Krabi.jpg",
-          titulo: "praia 2",
-        },
-      ],
+      fotos: [],
     };
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fts => (this.fotos = fts));
   },
 };
 </script>
