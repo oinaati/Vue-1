eca13fa965198ab99acd9c3367f517d61ef576dd
oinaati <natimartins2007@hotmail.com>
2022-04-20 13:47:15 -0300

    arrumado e barra de pesquisar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <ul>
-      <li v-for="(foto, i) of fotos" :key="i">
-        <img :src="foto.url" :alt="foto.titulo" />
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título">
+
+    <ul class="lista-fotos">
+      <!-- v-for="foto of fotos" -->
+      <li class="lista-fotos-item" v-for="(foto, i) of fotos" :key="i">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
+
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue"
+
 export default {
+
+  components: {
+    'meu-painel' : Painel
+  },
+
   data() {
     return {
       titulo: "Paisagens",
       fotos: [],
+      filtro: '',
     };
   },
 
   created() {
     this.$http
       .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(fts => (this.fotos = fts));
+      .then((res) => res.json())
+      .then((fts) => (this.fotos = fts));
   },
 };
 </script>
 
 <style>
+.corpo {
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+.centralizado {
+  text-align: center;
+}
+.lista-fotos {
+  list-style: none;
+}
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+.filtro {
+  width: 100%;
+  box-shadow: 2px 2px 3px;
+  border: solid 1px gray;
+  display: block;
+}
 </style>
